9d203f5326aa816179d705c3ec69196416583e4c
ovkpochta <120414224+ovkpochta@users.noreply.github.com>
2026-03-15 18:02:46 +0300

    Add Material Design dependency to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,6 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
 }
+dependencies {
+    implementation 'com.google.android.material:material:1.11.0'
+}
